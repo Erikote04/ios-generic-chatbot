03a02dd2141fd1efcc65131556aefe6738d4a9fd
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2026-08-19 12:37:43 +0200

    chore(app): init project

---
+++ ios-chatbot-sample-app/ios-chatbot-app/ios-chatbot-app/ios_chatbot_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_chatbot_appApp.swift
+//  ios-chatbot-app
+//
+//  Created by Erik Sebastian de Erice Jerez on 19/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct ios_chatbot_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
